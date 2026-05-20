35db77aa3c0aee4933aa0043614f4d382b3bde62
IdiotCoffee <ishaanmavinkurve16@gmail.com>
2026-05-20 22:17:17 +0530

    instead of docker commands running through shell, they now run through the docker library, making code much easier.

--- resolver/network.go
+++ resolver/network.go
@@ -3,57 +3,85 @@
 package resolver
 
 import (
-	"encoding/json"
-	"os/exec"
-	"strings"
+	"context"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
-type DockerInspect []struct {
-	NetworkSettings struct {
-		Networks map[string]struct {
-			IPAddress string `json:"IPAddress"`
-		} `json:"Networks"`
-	} `json:"NetworkSettings"`
+// making a docker client connector to connect to docker via a socket.
 
-	Config struct {
-		Labels map[string]string `json:"Labels"`
-	} `json:"Config"`
+func NewDockerClient() (*client.Client, error) {
+	return client.NewClientWithOpts(
+		client.FromEnv,
+		client.WithAPIVersionNegotiation(),
+	)
 }
 
+// type DockerInspect []struct {
+// 	NetworkSettings struct {
+// 		Networks map[string]struct {
+// 			IPAddress string `json:"IPAddress"`
+// 		} `json:"Networks"`
+// 	} `json:"NetworkSettings"`
+
+// 	Config struct {
+// 		Labels map[string]string `json:"Labels"`
+// 	} `json:"Config"`
+// }
+
 func BuildIPServiceMap() (map[string]string, error) {
-	ipMap := make(map[string]string)
-
-	cmd := exec.Command("docker", "ps", "-q")
-
-	output, err := cmd.Output()
+	cli, err := NewDockerClient()
 	if err != nil {
-		return nil, err
+		log.Fatalln(err)
 	}
 
-	containerIDs := strings.Fields(string(output))
+	ipMap := make(map[string]string)
 
-	for _, id := range containerIDs {
-		inspectCmd := exec.Command("docker", "inspect", id)
+	// cmd := exec.Command("docker", "ps", "-q")
+	// repacing the old shell command method with actual docker client library code
+	containers, err := cli.ContainerList(
+		context.Background(),
+		types.ContainerListOptions{},
+	)
 
-		inspectOutput, err := inspectCmd.Output()
+	// output, err := cmd.Output()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// containerIDs := strings.Fields(string(containers))
+	for _, c := range containers {
+		// inspectCmd := exec.Command("docker", "inspect", id)
+		inspect, err := cli.ContainerInspect(
+			context.Background(),
+			c.ID,
+		)
 		if err != nil {
+			log.Println(err)
 			continue
 		}
 
-		var data DockerInspect
+		// inspectOutput, err := inspectCmd.Output()
+		// if err != nil {
+		// 	continue
+		// }
 
-		err = json.Unmarshal(inspectOutput, &data)
-		if err != nil {
-			continue
-		}
+		// var data DockerInspect
 
-		if len(data) == 0 {
-			continue
-		}
+		// err = json.Unmarshal(inspect, &data)
+		// if err != nil {
+		// 	continue
+		// }
 
-		service := data[0].Config.Labels["com.docker.compose.service"]
+		// if len(data) == 0 {
+		// 	continue
+		// }
 
-		for _, network := range data[0].NetworkSettings.Networks {
+		service := inspect.Config.Labels["com.docker.compose.service"]
+
+		for _, network := range inspect.NetworkSettings.Networks {
 			ip := network.IPAddress
 
 			if ip != "" && service != "" {
